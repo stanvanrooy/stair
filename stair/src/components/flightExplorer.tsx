89c45fff42963c67037ee3c043f5a496d2f760c9
Stan van Rooy <stan@rooy.dev>
2021-11-02 00:47:27 +0000

    feat: add sort by filter

--- stair/src/components/flightExplorer.tsx
+++ stair/src/components/flightExplorer.tsx
@@ -1,18 +1,64 @@
+import { TextField } from "@fluentui/react";
+import { useMemo } from "react";
+import { numberOptions, useQueryState } from "../helpers";
 import { Flight } from "../models/flight"
 
 import styles from "./flightExplorer.module.css"
 import { FlightView } from "./flightView"
+import { SortSelect } from "./sortSelect";
 
 export interface IFlightExplorerProps {
   flights: Flight[]
 }
 
 export const FlightExplorer = (props: IFlightExplorerProps) => {
+  const [selectedSort, setSelectedSort] = useQueryState<string>('sort', 'price');
+  const [maxDuration, setMaxDuration] = useQueryState<number | null>('maxdur', null, numberOptions);
+
+  const sortedFlights = useMemo(() => {
+    let flights = [];
+    if (selectedSort === 'price') {
+      flights = props.flights;
+    } else if (selectedSort === 'duration') {
+      flights = props.flights.sort((a, b) => {
+        const ad = a.duration.total;
+        const bd = b.duration.total;
+        if (ad === bd) return 0;
+        if (ad < bd) return -1;
+        return 1;
+      });
+    }
+    if (maxDuration === null) {
+      return flights;
+    }
+    return flights.filter(f => {
+      const valid = (s: number) => {
+        if (s === 0)
+          return true;
+        return s / 60 / 60 < maxDuration;
+      }
+      return valid(f.duration.departure) && valid(f.duration.return);
+    });
+  }, [selectedSort, props.flights, maxDuration])
+
   if (props.flights === null) {
     return null;
   }
 
   return <div className={styles.container}>
-    {props.flights.map(f => <FlightView flight={f} key={f.id} />)}
+    <br />
+    <div className={styles.filters}>
+      <SortSelect value={selectedSort} onChange={setSelectedSort} />
+      <TextField
+        label={"Max. duration"}
+        type={"number"}
+        value={maxDuration?.toString()}
+        onChange={(_: any, nv?: string) => setMaxDuration(nv && parseInt(nv) > 0 ? parseInt(nv) : null)}
+        suffix={maxDuration && maxDuration > 1 ? "hours" : "hour"}
+        min={1}
+      />
+    </div>
+    <br />
+    {sortedFlights.map(f => <FlightView flight={f} key={f.id} />)}
   </div>
 }
---
+++ stair/src/components/sortSelect.tsx
@@ -0,0 +1,20 @@
+import { Dropdown, IDropdownOption } from "@fluentui/react"
+
+export interface ISortSelectProps {
+  value: string;
+  onChange: (s: string) => void;
+}
+
+const options = [
+  {key: 'price', text: 'Price'},
+  {key: 'duration', text: 'Duration'},
+]
+
+export const SortSelect = (props: ISortSelectProps) => {
+  return <Dropdown
+    options={options}
+    selectedKey={props.value}
+    onChange={(_: any, opt: IDropdownOption) => props.onChange(opt.key.toString())}
+    label={"Sort by"}
+  />
+}
